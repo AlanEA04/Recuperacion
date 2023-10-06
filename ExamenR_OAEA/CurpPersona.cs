c54ecef41b6fb87cf859342d37f7887fa523a5c4
AlanEA04 <138253309+AlanEA04@users.noreply.github.com>
2023-10-06 09:49:36 -0700

    Add files via upload

---
+++ ExamenR_OAEA/CurpPersona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenR_OAEA
+{
+    internal class CurpPersona
+    {
+        //* public string  ObtenerSexo(string curp)
+        //{
+
+          /*  char SexoChar = curp[10];
+            return (SexoChar == "H" || SexoChar == "h" )?"Nombre" : "Mujer";
+
+        }*/
+        
+
+
+
+        public string ObtenerFechaNacimiento(string curp)
+        {
+
+            string dia = curp.Substring(8,2);
+            string mes = curp.Substring(6,2);
+            string año = curp.Substring(4,2);
+            return dia + "/" + mes + "/19" + año; 
+
+        }
+
+
+      
+
+
+    }
+}
---
+++ ExamenR_OAEA/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ExamenR_OAEA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa tu Curp");
+            string Curp = (Console.ReadLine());
+
+            CurpPersona curpPersona= new CurpPersona();
+
+
+
+
+
+           // string sexo = curpPersona.ObtenerSexo(Curp);
+
+            string fechaNaciminto = curpPersona.ObtenerFechaNacimiento(Curp);
+
+
+          //  Console.WriteLine("Sexo" + sexo);
+            Console.WriteLine("FechaNacimiento: " + fechaNaciminto);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
